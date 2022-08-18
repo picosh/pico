159c345c3e91a4f4608b879306a21c48c33686c6
Eric Bower <me@erock.io>
2022-08-17 22:42:18 -0400

    fix(sql): updated posts table columns to fix regression
    We did not assume mime_type or shasum could be null, so this adjusts the
    migration to use what we pasted into our prod db.

--- sql/migrations/20220811_add_data_to_post.sql
+++ sql/migrations/20220811_add_data_to_post.sql
@@ -1,4 +1,4 @@
-ALTER TABLE posts ADD COLUMN shasum char(64);
-ALTER TABLE posts ADD COLUMN mime_type char(32);
+ALTER TABLE posts ADD COLUMN shasum char(64) NOT NULL DEFAULT '';
+ALTER TABLE posts ADD COLUMN mime_type character varying(250) NOT NULL DEFAULT '';
 ALTER TABLE posts ADD COLUMN file_size int NOT NULL DEFAULT 0;
 ALTER TABLE posts ADD COLUMN data jsonb NOT NULL DEFAULT '{}'::jsonb;
