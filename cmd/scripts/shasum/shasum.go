691db4300d54a101f5bba4908dd16319d72ac797
Eric Bower <me@erock.io>
2022-08-22 23:23:23 -0400

    refactor(all): making sure every post has shasum

---
+++ cmd/scripts/shasum/shasum.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"git.sr.ht/~erock/pico/db/postgres"
+	"git.sr.ht/~erock/pico/shared"
+	"git.sr.ht/~erock/pico/wish/cms/config"
+	"go.uber.org/zap"
+)
+
+func createLogger() *zap.SugaredLogger {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return logger.Sugar()
+}
+
+func main() {
+	logger := createLogger()
+	picoCfg := config.NewConfigCms()
+	picoCfg.Logger = logger
+	picoCfg.DbURL = os.Getenv("DATABASE_URL")
+	picoDb := postgres.NewDB(picoCfg)
+
+	logger.Info("fetching all posts")
+	posts, err := picoDb.FindPosts()
+	if err != nil {
+		panic(err)
+	}
+
+	empty := 0
+	diff := 0
+	for _, post := range posts {
+		nextShasum := shared.Shasum([]byte(post.Text))
+		if post.Shasum == "" {
+			empty += 1
+		} else if post.Shasum != nextShasum {
+			diff += 1
+		}
+		post.Shasum = nextShasum
+
+		_, err := picoDb.UpdatePost(post)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	logger.Infof("empty (%d), diff (%d)", empty, diff)
+}
