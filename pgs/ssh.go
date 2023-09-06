2efaeedfd7122645c3543a3e5e7312888d655ba7
Eric Bower <me@erock.io>
2023-09-05 21:03:40 -0400

    chore(pgs): move most ssh code to pgs package

---
+++ pgs/ssh.go
@@ -0,0 +1,119 @@
+package pgs
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/charmbracelet/promwish"
+	"github.com/charmbracelet/wish"
+	bm "github.com/charmbracelet/wish/bubbletea"
+	lm "github.com/charmbracelet/wish/logging"
+	"github.com/gliderlabs/ssh"
+	"github.com/picosh/pico/db/postgres"
+	uploadassets "github.com/picosh/pico/filehandlers/assets"
+	"github.com/picosh/pico/shared"
+	"github.com/picosh/pico/shared/storage"
+	"github.com/picosh/pico/wish/list"
+	"github.com/picosh/pico/wish/pipe"
+	"github.com/picosh/pico/wish/proxy"
+	"github.com/picosh/pico/wish/send/auth"
+	wishrsync "github.com/picosh/pico/wish/send/rsync"
+	"github.com/picosh/pico/wish/send/scp"
+	"github.com/picosh/pico/wish/send/sftp"
+)
+
+type SSHServer struct{}
+
+func (me *SSHServer) authHandler(ctx ssh.Context, key ssh.PublicKey) bool {
+	return true
+}
+
+func createRouter(cfg *shared.ConfigSite, handler *uploadassets.UploadAssetHandler) proxy.Router {
+	return func(sh ssh.Handler, s ssh.Session) []wish.Middleware {
+		return []wish.Middleware{
+			pipe.Middleware(handler, ""),
+			list.Middleware(handler),
+			WishMiddleware(handler),
+			scp.Middleware(handler),
+			wishrsync.Middleware(handler),
+			auth.Middleware(handler),
+			bm.Middleware(CmsMiddleware(&cfg.ConfigCms, cfg)),
+			lm.Middleware(),
+		}
+	}
+}
+
+func withProxy(cfg *shared.ConfigSite, handler *uploadassets.UploadAssetHandler, otherMiddleware ...wish.Middleware) ssh.Option {
+	return func(server *ssh.Server) error {
+		err := sftp.SSHOption(handler)(server)
+		if err != nil {
+			return err
+		}
+
+		return proxy.WithProxy(createRouter(cfg, handler), otherMiddleware...)(server)
+	}
+}
+
+func StartSshServer() {
+	host := shared.GetEnv("PGS_HOST", "0.0.0.0")
+	port := shared.GetEnv("PGS_SSH_PORT", "2222")
+	promPort := shared.GetEnv("PGS_PROM_PORT", "9222")
+	cfg := NewConfigSite()
+	logger := cfg.Logger
+	dbh := postgres.NewDB(&cfg.ConfigCms)
+	defer dbh.Close()
+
+	var st storage.ObjectStorage
+	var err error
+	if cfg.MinioURL == "" {
+		st, err = storage.NewStorageFS(cfg.StorageDir)
+	} else {
+		st, err = storage.NewStorageMinio(cfg.MinioURL, cfg.MinioUser, cfg.MinioPass)
+	}
+
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	handler := uploadassets.NewUploadAssetHandler(
+		dbh,
+		cfg,
+		st,
+	)
+
+	sshServer := &SSHServer{}
+	s, err := wish.NewServer(
+		wish.WithAddress(fmt.Sprintf("%s:%s", host, port)),
+		wish.WithHostKeyPath("ssh_data/term_info_ed25519"),
+		wish.WithPublicKeyAuth(sshServer.authHandler),
+		withProxy(
+			cfg,
+			handler,
+			promwish.Middleware(fmt.Sprintf("%s:%s", host, promPort), "pgs-ssh"),
+		),
+	)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	logger.Infof("Starting SSH server on %s:%s", host, port)
+	go func() {
+		if err = s.ListenAndServe(); err != nil {
+			logger.Fatal(err)
+		}
+	}()
+
+	<-done
+	logger.Info("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := s.Shutdown(ctx); err != nil {
+		logger.Fatal(err)
+	}
+}
