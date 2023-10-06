539fa6ee6ba4998a49e8fedcdd34693163dec04c
Antonio Mika <antoniomika@gmail.com>
2023-10-06 09:42:43 -0400

    Implement a fake oauth login system that uses ssh generated tokens

--- auth/auth.go
+++ auth/auth.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"net/http"
+	"net/url"
 	"strings"
 
 	"github.com/picosh/pico/db"
@@ -29,22 +31,27 @@ type oauth2Server struct {
 	Issuer                                    string   `json:"issuer"`
 	IntrospectionEndpoint                     string   `json:"introspection_endpoint"`
 	IntrospectionEndpointAuthMethodsSupported []string `json:"introspection_endpoint_auth_methods_supported"`
+	AuthorizationEndpoint                     string   `json:"authorization_endpoint"`
+	TokenEndpoint                             string   `json:"token_endpoint"`
+	ResponseTypesSupported                    []string `json:"response_types_supported"`
 }
 
-func getIntrospectURL(cfg *AuthCfg) string {
-	return fmt.Sprintf("%s/introspect", cfg.Domain)
+func generateURL(cfg *AuthCfg, path string) string {
+	return fmt.Sprintf("%s/%s", cfg.Domain, path)
 }
 
 func wellKnownHandler(w http.ResponseWriter, r *http.Request) {
 	client := getClient(r)
-	introspectURL := getIntrospectURL(client.Cfg)
 
 	p := oauth2Server{
 		Issuer:                client.Cfg.Issuer,
-		IntrospectionEndpoint: introspectURL,
+		IntrospectionEndpoint: generateURL(client.Cfg, "introspect"),
 		IntrospectionEndpointAuthMethodsSupported: []string{
 			"none",
 		},
+		AuthorizationEndpoint:  generateURL(client.Cfg, "authorize"),
+		TokenEndpoint:          generateURL(client.Cfg, "token"),
+		ResponseTypesSupported: []string{"code"},
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
@@ -85,10 +92,119 @@ func introspectHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func authorizeHandler(w http.ResponseWriter, r *http.Request) {
+	client := getClient(r)
+
+	responseType := r.URL.Query().Get("response_type")
+	clientID := r.URL.Query().Get("client_id")
+	redirectURI := r.URL.Query().Get("redirect_uri")
+	scope := r.URL.Query().Get("scope")
+
+	client.Logger.Infof("authorize handler (%s, %s, %s, %s)", responseType, clientID, redirectURI, scope)
+
+	ts, err := template.ParseFiles(
+		"auth/html/redirect.page.tmpl",
+		"auth/html/footer.partial.tmpl",
+		"auth/html/marketing-footer.partial.tmpl",
+		"auth/html/base.layout.tmpl",
+	)
+
+	if err != nil {
+		client.Logger.Error(err)
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	err = ts.Execute(w, map[string]any{
+		"response_type": responseType,
+		"client_id":     clientID,
+		"redirect_uri":  redirectURI,
+		"scope":         scope,
+	})
+
+	if err != nil {
+		client.Logger.Error(err)
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	client := getClient(r)
+
+	token := r.FormValue("token")
+	redirectURI := r.FormValue("redirect_uri")
+	responseType := r.FormValue("response_type")
+
+	client.Logger.Infof("redirect handler (%s, %s, %s)", token, redirectURI, responseType)
+
+	if token == "" || redirectURI == "" || responseType != "code" {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	url, err := url.Parse(redirectURI)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	urlQuery := url.Query()
+	urlQuery.Add("code", token)
+
+	url.RawQuery = urlQuery.Encode()
+
+	http.Redirect(w, r, url.String(), http.StatusFound)
+}
+
+type oauth2Token struct {
+	AccessToken string `json:"access_token"`
+}
+
+func tokenHandler(w http.ResponseWriter, r *http.Request) {
+	client := getClient(r)
+
+	token := r.FormValue("code")
+	redirectURI := r.FormValue("redirect_uri")
+	grantType := r.FormValue("grant_type")
+
+	client.Logger.Infof("handle token (%s, %s, %s)", token, redirectURI, grantType)
+
+	_, err := client.Dbpool.FindUserForToken(token)
+	if err != nil {
+		client.Logger.Error(err)
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	p := oauth2Token{
+		AccessToken: token,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	err = json.NewEncoder(w).Encode(p)
+	if err != nil {
+		client.Logger.Error(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func createMainRoutes() []shared.Route {
+	fileServer := http.FileServer(http.Dir("auth/public"))
+
 	routes := []shared.Route{
 		shared.NewRoute("GET", "/.well-known/oauth-authorization-server", wellKnownHandler),
 		shared.NewRoute("POST", "/introspect", introspectHandler),
+		shared.NewRoute("GET", "/authorize", authorizeHandler),
+		shared.NewRoute("POST", "/token", tokenHandler),
+		shared.NewRoute("POST", "/redirect", redirectHandler),
+		shared.NewRoute("GET", "/main.css", fileServer.ServeHTTP),
+		shared.NewRoute("GET", "/card.png", fileServer.ServeHTTP),
+		shared.NewRoute("GET", "/favicon-16x16.png", fileServer.ServeHTTP),
+		shared.NewRoute("GET", "/favicon-32x32.png", fileServer.ServeHTTP),
+		shared.NewRoute("GET", "/apple-touch-icon.png", fileServer.ServeHTTP),
+		shared.NewRoute("GET", "/favicon.ico", fileServer.ServeHTTP),
+		shared.NewRoute("GET", "/robots.txt", fileServer.ServeHTTP),
 	}
 
 	return routes
