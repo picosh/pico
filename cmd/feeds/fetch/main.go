f00e329d4cb38edc8f2134490661f8a95bc9fe34
Eric Bower <me@erock.io>
2024-01-21 23:33:21 -0500

    chore: add feeds cmd for testing fetcher

---
+++ cmd/feeds/fetch/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mmcdole/gofeed"
+	"github.com/picosh/pico/db/postgres"
+	"github.com/picosh/pico/feeds"
+)
+
+func main() {
+	cfg := feeds.NewConfigSite()
+	dbh := postgres.NewDB(cfg.DbURL, cfg.Logger)
+	defer dbh.Close()
+	fetcher := feeds.NewFetcher(dbh, cfg)
+	fp := gofeed.NewParser()
+	feed, err := fetcher.ParseURL(fp, "https://old.reddit.com/r/Watchexchange/.rss")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(feed)
+}
