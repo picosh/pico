e42f99bf550414a6de2041a817d73d2fe307b233
Eric Bower <me@erock.io>
2024-02-18 12:13:04 -0500

    chore: update pico+ sql

--- sql/pico_plus_user.sql
+++ sql/pico_plus_user.sql
@@ -7,19 +7,24 @@ SELECT id FROM app_users WHERE name = '{user}';
 INSERT INTO payment_history (user_id, payment_type, amount, data)
 VALUES ('', 'stripe', 20 * 1000000, '{"notes": ""}'::jsonb) RETURNING id;
 
--- enable pro features
+-- enable pico+ features
 
 -- pgs
 -- storage max is 10gb
 -- file max is 50mb
 INSERT INTO feature_flags (user_id, name, data, expires_at)
-VALUES ('', 'pgs', '{"storage_max":10737418240, "file_max":52428800}'::jsonb, now() + '1 year'::interval);
+VALUES ('', 'pgs', '{"storage_max":10000000000, "file_max":50000000}'::jsonb, now() + '1 year'::interval);
 
 -- imgs
--- storage max is 10gb
+-- storage max is 2gb
+INSERT INTO feature_flags (user_id, name, data, expires_at)
+VALUES ('', 'imgs', '{"storage_max":2000000000}'::jsonb, now() + '1 year'::interval);
+
+-- prose
+-- storage max is 1gb
 -- file max is 50mb
 INSERT INTO feature_flags (user_id, name, data, expires_at)
-VALUES ('', 'imgs', '{"storage_max":10737418240, "file_max":52428800}'::jsonb, now() + '1 year'::interval);
+VALUES ('', 'pgs', '{"storage_max":2000000000, "file_max":50000000}'::jsonb, now() + '1 year'::interval);
 
 -- tuns
 INSERT INTO feature_flags (user_id, name, expires_at)
