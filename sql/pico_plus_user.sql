2023f814f0beb96b1b5d8df050dbab657e22d03b
Eric Bower <me@erock.io>
2024-02-18 21:10:02 -0500

    fix: pico+ sql

--- sql/pico_plus_user.sql
+++ sql/pico_plus_user.sql
@@ -5,7 +5,7 @@ SELECT id FROM app_users WHERE name = '{user}';
 -- amount should be multiplied by 1 million and then later divded by the same
 -- https://stackoverflow.com/a/51238749
 INSERT INTO payment_history (user_id, payment_type, amount, data)
-VALUES ('', 'stripe', 20 * 1000000, '{"notes": ""}'::jsonb) RETURNING id;
+VALUES ('', 'stripe', 20 * 1000000, '{"notes": "", "tx_id":""}'::jsonb) RETURNING id;
 
 -- enable pico+ features
 
@@ -24,7 +24,7 @@ VALUES ('', 'imgs', '{"storage_max":2000000000}'::jsonb, now() + '1 year'::inter
 -- storage max is 1gb
 -- file max is 50mb
 INSERT INTO feature_flags (user_id, name, data, expires_at)
-VALUES ('', 'pgs', '{"storage_max":2000000000, "file_max":50000000}'::jsonb, now() + '1 year'::interval);
+VALUES ('', 'prose', '{"storage_max":1000000000, "file_max":50000000}'::jsonb, now() + '1 year'::interval);
 
 -- tuns
 INSERT INTO feature_flags (user_id, name, expires_at)
