a932d536738d0153afee5418b0535891ed306130
Eric Bower <me@erock.io>
2024-03-01 09:22:08 -0500

    feat(pgs): jwt

--- pgs/jwt.go
+++ pgs/jwt.go
@@ -9,41 +9,37 @@ import (
 
 var hmacSampleSecret = []byte("picopicopico")
 
-func CreateToken() {
+func CreateToken(userID, username, pubkey string) (string, error) {
 	// Create a new token object, specifying signing method and the claims
 	// you would like it to contain.
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"foo": "bar",
-		"nbf": time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+		"username": username,
+		"pubkey":   pubkey,
+		"nbf":      time.Now().Add(time.Minute * 5 * -1).Unix(),
+		"iat":      time.Now().Unix(),
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+		"iss":      "pico.sh",
+		"sub":      userID,
 	})
 
 	// Sign and get the complete encoded token as a string using the secret
-	tokenString, err := token.SignedString(hmacSampleSecret)
-
-	fmt.Println(tokenString, err)
+	return token.SignedString(hmacSampleSecret)
 }
 
-func IsTokenValid(tokenString string) (jwt.MapClaims, error) {
+func keyFunc(token *jwt.Token) (interface{}, error) {
+	// Don't forget to validate the alg is what you expect:
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+	}
+
+	// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+	return hmacSampleSecret, nil
+}
+
+func IsTokenValid(tokenString string) (*jwt.Token, error) {
 	// Parse takes the token string and a function for looking up the key. The latter is especially
 	// useful if you use multiple keys for your application.  The standard is to use 'kid' in the
 	// head of the token to identify which key to use, but the parsed token (head and claims) is provided
 	// to the callback, providing flexibility.
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
-
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
-		return hmacSampleSecret, nil
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	if claims, ok := token.Claims.(jwt.MapClaims); ok {
-		return claims, nil
-	} else {
-		return nil, err
-	}
+	return jwt.Parse(tokenString, keyFunc)
 }
