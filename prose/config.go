74acb5a840672c13c6274adfa8b39b7ce52fe6eb
Eric Bower <me@erock.io>
2024-05-07 10:02:40 -0400

    fix(prose): allow .ico to be uploaded
    Closes: #131

--- prose/config.go
+++ prose/config.go
@@ -41,6 +41,7 @@ func NewConfigSite() *shared.ConfigSite {
 			".gif",
 			".webp",
 			".svg",
+			".ico",
 		},
 		HiddenPosts:  []string{"_readme.md", "_styles.css", "_footer.md", "_404.md"},
 		Logger:       shared.CreateLogger(debug == "1"),
