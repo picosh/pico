5751e642b46200af487ffec0486cf07f91cb36ef
Antonio Mika <antoniomika@gmail.com>
2024-10-03 17:45:33 -0400

    Enable robust log handler

--- shared/sendlog.go
+++ shared/sendlog.go
@@ -1,6 +1,7 @@
 package shared
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"log/slog"
@@ -16,40 +17,110 @@ import (
 )
 
 type SendLogWriter struct {
-	SSHClient *ssh.Client
-	Session   *ssh.Session
-	StdinPipe io.Writer
-	Done      chan struct{}
-	Messages  chan []byte
-	Timeout   time.Duration
-	closeOnce sync.Once
-	startOnce sync.Once
+	SSHClient  *ssh.Client
+	Session    *ssh.Session
+	StdinPipe  io.WriteCloser
+	Done       chan struct{}
+	Messages   chan []byte
+	Timeout    time.Duration
+	BufferSize int
+	closeOnce  sync.Once
+	startOnce  sync.Once
+	connecMu   sync.Mutex
 }
 
-func (c *SendLogWriter) Write(data []byte) (int, error) {
-	var (
-		n   int
-		err error
-	)
+func (c *SendLogWriter) Close() error {
+	c.connecMu.Lock()
+	defer c.connecMu.Unlock()
 
-	select {
-	case c.Messages <- data:
-		n = len(data)
-	case <-time.After(c.Timeout):
-		err = fmt.Errorf("unable to send data within timeout")
+	if c.Done != nil {
+		close(c.Done)
 	}
 
-	return n, err
+	if c.Messages != nil {
+		close(c.Messages)
+	}
+
+	var errs []error
+
+	if c.StdinPipe != nil {
+		errs = append(errs, c.StdinPipe.Close())
+	}
+
+	if c.Session != nil {
+		errs = append(errs, c.Session.Close())
+	}
+
+	if c.SSHClient != nil {
+		errs = append(errs, c.SSHClient.Close())
+	}
+
+	return errors.Join(errs...)
 }
 
-func (c *SendLogWriter) Open() {
+func (c *SendLogWriter) Open() error {
+	c.Close()
+
+	c.connecMu.Lock()
+
+	c.Done = make(chan struct{})
+	c.Messages = make(chan []byte, c.BufferSize)
+
+	sshClient, err := createSSHClient(
+		"send.pico.sh:22",
+		"ssh_data/term_info_ed25519",
+		"",
+		"send.pico.sh",
+		"pico",
+	)
+	if err != nil {
+		c.connecMu.Unlock()
+		return err
+	}
+
+	session, err := sshClient.NewSession()
+	if err != nil {
+		c.connecMu.Unlock()
+		return err
+	}
+
+	stdinPipe, err := session.StdinPipe()
+	if err != nil {
+		c.connecMu.Unlock()
+		return err
+	}
+
+	err = session.Start("pub log-sink -b=false")
+	if err != nil {
+		c.connecMu.Unlock()
+		return err
+	}
+
+	c.SSHClient = sshClient
+	c.Session = session
+	c.StdinPipe = stdinPipe
+
+	c.closeOnce = sync.Once{}
+	c.startOnce = sync.Once{}
+
+	c.connecMu.Unlock()
+
+	c.Start()
+
+	return nil
+}
+
+func (c *SendLogWriter) Start() {
 	go func() {
+		defer c.Reconnect()
+
 		for {
 			select {
-			case data := <-c.Messages:
+			case data, ok := <-c.Messages:
 				_, err := c.StdinPipe.Write(data)
-				if err != nil {
-					slog.Info("received error on write", "error", err)
+				if !ok || err != nil {
+					slog.Error("received error on write, reopening logger", "error", err)
+					return
 				}
 			case <-c.Done:
 				return
@@ -58,46 +129,70 @@ func (c *SendLogWriter) Open() {
 	}()
 }
 
-func createSSHClient(remoteHost string, keyLocation string, keyPassphrase string, remoteHostname string, remoteUser string) *ssh.Client {
+func (c *SendLogWriter) Write(data []byte) (int, error) {
+	c.connecMu.Lock()
+	defer c.connecMu.Unlock()
+
+	var (
+		n   int
+		err error
+	)
+
+	if c.Messages == nil || c.Done == nil {
+		return n, fmt.Errorf("logger not viable")
+	}
+
+	select {
+	case c.Messages <- data:
+		n = len(data)
+	case <-time.After(c.Timeout):
+		err = fmt.Errorf("unable to send data within timeout")
+	case <-c.Done:
+		break
+	}
+
+	return n, err
+}
+
+func (c *SendLogWriter) Reconnect() {
+	go func() {
+		for {
+			err := c.Open()
+			if err != nil {
+				slog.Error("unable to open send logger. retrying in 10 seconds", "error", err)
+			} else {
+				return
+			}
+
+			<-time.After(10 * time.Second)
+		}
+	}()
+}
+
+func createSSHClient(remoteHost string, keyLocation string, keyPassphrase string, remoteHostname string, remoteUser string) (*ssh.Client, error) {
 	if !strings.Contains(remoteHost, ":") {
 		remoteHost += ":22"
 	}
 
 	rawConn, err := net.Dial("tcp", remoteHost)
 	if err != nil {
-		slog.Error(
-			"Unable to create ssh client, tcp connection not established",
-			slog.Any("error", err),
-		)
-		panic(err)
+		return nil, err
 	}
 
 	keyPath, err := filepath.Abs(keyLocation)
 	if err != nil {
-		slog.Error(
-			"Unable to create ssh client, cannot find key file",
-			slog.Any("error", err),
-		)
-		panic(err)
+		return nil, err
 	}
 
 	f, err := os.Open(keyPath)
 	if err != nil {
-		slog.Error(
-			"Unable to create ssh client, unable to open key",
-			slog.Any("error", err),
-		)
-		panic(err)
+		return nil, err
 	}
 	defer f.Close()
 
 	data, err := io.ReadAll(f)
 	if err != nil {
-		slog.Error(
-			"Unable to create ssh client, unable to read key",
-			slog.Any("error", err),
-		)
-		panic(err)
+		return nil, err
 	}
 
 	var signer ssh.Signer
@@ -109,11 +204,7 @@ func createSSHClient(remoteHost string, keyLocation string, keyPassphrase string
 	}
 
 	if err != nil {
-		slog.Error(
-			"Unable to create ssh client, unable to parse key",
-			slog.Any("error", err),
-		)
-		panic(err)
+		return nil, err
 	}
 
 	sshConn, chans, reqs, err := ssh.NewClientConn(rawConn, remoteHostname, &ssh.ClientConfig{
@@ -121,17 +212,14 @@ func createSSHClient(remoteHost string, keyLocation string, keyPassphrase string
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 		User:            remoteUser,
 	})
+
 	if err != nil {
-		slog.Error(
-			"Unable to create ssh client, unable to create client conn",
-			slog.Any("error", err),
-		)
-		panic(err)
+		return nil, err
 	}
 
 	sshClient := ssh.NewClient(sshConn, chans, reqs)
 
-	return sshClient
+	return sshClient, nil
 }
 
 func SendLogRegister(logger *slog.Logger, buffer int) (*slog.Logger, error) {
@@ -141,39 +229,12 @@ func SendLogRegister(logger *slog.Logger, buffer int) (*slog.Logger, error) {
 
 	currentHandler := logger.Handler()
 
-	sshClient := createSSHClient(
-		"send.pico.sh:22",
-		os.Getenv("SSH_KEY"),
-		os.Getenv("SSH_PASSPHRASE"),
-		"send.pico.sh",
-		os.Getenv("SSH_USER"),
-	)
-
-	sesh, err := sshClient.NewSession()
-	if err != nil {
-		return logger, nil
-	}
-
-	stdinPipe, err := sesh.StdinPipe()
-	if err != nil {
-		return logger, nil
-	}
-
-	err = sesh.Start("pub log-sink -b=false")
-	if err != nil {
-		return logger, nil
-	}
-
 	logWriter := &SendLogWriter{
-		SSHClient: sshClient,
-		Session:   sesh,
-		StdinPipe: stdinPipe,
-		Done:      make(chan struct{}),
-		Messages:  make(chan []byte, buffer),
-		Timeout:   10 * time.Millisecond,
+		Timeout:    10 * time.Millisecond,
+		BufferSize: buffer,
 	}
 
-	logWriter.Open()
+	logWriter.Reconnect()
 
 	return slog.New(
 		slogmulti.Fanout(
