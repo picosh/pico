ac3be1722d7dbfb69fe98697af2bd071fa3dbf0c
Eric Bower <me@erock.io>
2024-10-29 00:12:36 -0400

    chore(analytics): add indexes to table

---
+++ sql/migrations/20241028_add_analytics_indexes.sql
@@ -0,0 +1,3 @@
+CREATE INDEX analytics_visits_created_at_date_idx ON analytics_visits (created_at);
+CREATE INDEX analytics_visits_status_idx ON analytics_visits (status);
+CREATE INDEX analytics_visits_user_id_idx ON analytics_visits (user_id);
