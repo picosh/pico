6a7c21b947585f786dd0f688189d1295b341b7ef
Eric Bower <me@erock.io>
2024-12-23 20:34:29 -0500

    chore(script): change col names

--- scripts/mau.sql
+++ scripts/mau.sql
@@ -1,19 +1,19 @@
 select
-  count(distinct user_id) as mau_posts,
+  count(distinct user_id) as active_posts,
   date_trunc('month', updated_at) as month
 from posts
 group by month
 order by month DESC;
 
 select
-  count(distinct user_id) as mau_pgs,
+  count(distinct user_id) as active_sites,
   date_trunc('month', updated_at) as month
 from projects
 group by month
 order by month DESC;
 
 select
-  count(id) as users,
+  count(id) as new_users,
   date_trunc('month', created_at) as month
 from app_users
 group by month
