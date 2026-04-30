c9c8c0f7f37d6ba9df4b0243ab196332754a83ef
Eric Bower <me@erock.io>
2026-04-29 23:20:54 -0400

    chore: tweaks

--- pico.sh
+++ pico.sh
@@ -3,19 +3,24 @@ set -xeuo pipefail
 
 export ZMX_SESSION_PREFIX="ci-"
 
-DOCKER_TAG="latest"
-DOCKER_PLATFORM="linux/amd64,linux/arm64"
-
-docker buildx ls | grep pico || docker buildx create --name pico
-docker buildx use pico
-
 cat << EOF | zmx run tests -d
 docker build -t pico-test -f ./Dockerfile.test . && \
 docker run -t --rm -v $(pwd):/app pico-test && \
 docker run -t --rm -v $(pwd):/app -w /app golangci/golangci-lint:v2.11.4 golangci-lint run
 EOF
 zmx wait "*"
+zmx kill "*"
+printf "\x1b[32msuccess tests!\x1b[0m"
 
+if [ "$1" != "release" ]; then
+  exit 0
+fi
+
+DOCKER_TAG="latest"
+DOCKER_PLATFORM="linux/amd64,linux/arm64"
+
+docker buildx ls | grep pico || docker buildx create --name pico
+docker buildx use pico
 zmx run caddy -d       docker buildx build --platform "$DOCKER_PLATFORM" -t "ghcr.io/picosh/pico/caddy:$DOCKER_TAG" ./caddy
 zmx run auth -d        docker buildx build --platform "$DOCKER_PLATFORM" -t "ghcr.io/picosh/pico/auth-web:$DOCKER_TAG" --build-arg APP=auth --target release-web .
 zmx run cdn -d         docker buildx build --platform "$DOCKER_PLATFORM" -t "ghcr.io/picosh/pico/pgs-cdn:$DOCKER_TAG" --target release-web -f Dockerfile.cdn .
@@ -31,4 +36,4 @@ done
 zmx wait "*"
 
 zmx kill "*"
-echo "success!"
+printf "\x1b[32msuccess release!\x1b[0m"
