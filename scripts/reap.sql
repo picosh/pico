483a3a2ed8604987a061c339d3b0eb64f90bdfc6
Eric Bower <me@erock.io>
2026-05-03 13:02:30 -0400

    chore: delete old analytics

--- scripts/reap.sql
+++ scripts/reap.sql
@@ -1,1 +1,9 @@
+-- delete unused accounts
 SELECT count(*) FROM app_users u WHERE NOT EXISTS (SELECT 1 FROM posts WHERE user_id = u.id) AND NOT EXISTS (SELECT 1 FROM projects WHERE user_id = u.id) AND NOT EXISTS (SELECT 1 FROM access_logs WHERE user_id = u.id AND created_at > NOW() - INTERVAL '1 year');
+
+-- how many visits will be deleted
+SELECT count(*) FROM analytics_visits WHERE created_at < NOW() - INTERVAL '1 year';
+-- delete old analytic visits
+DELETE FROM analytics_visits WHERE created_at < NOW() - INTERVAL '1 year';
+-- batch delete
+WITH deleted AS (DELETE FROM analytics_visits WHERE ctid IN (SELECT ctid FROM analytics_visits WHERE created_at < NOW() - INTERVAL '1 year' LIMIT 100000) RETURNING 1) SELECT count(*) FROM deleted;
