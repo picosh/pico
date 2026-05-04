4cd5fc0db0d6617bcf9c2cb94b08672a8a95c023
Eric Bower <me@erock.io>
2026-05-03 22:54:47 -0400

    chore: script reap edits

--- scripts/reap.sql
+++ scripts/reap.sql
@@ -1,7 +1,7 @@
 -- find unused accounts
-SELECT count(*) FROM app_users u WHERE NOT EXISTS (SELECT 1 FROM posts WHERE user_id = u.id) AND NOT EXISTS (SELECT 1 FROM projects WHERE user_id = u.id) AND NOT EXISTS (SELECT 1 FROM access_logs WHERE user_id = u.id AND created_at > NOW() - INTERVAL '1 year') AND NOT EXISTS (SELECT 1 FROM feature_flags ff WHERE ff.user_id = u.id AND ff.name = 'plus');
+SELECT count(*) FROM app_users u WHERE NOT EXISTS (SELECT 1 FROM posts WHERE user_id = u.id) AND NOT EXISTS (SELECT 1 FROM projects WHERE user_id = u.id) AND NOT EXISTS (SELECT 1 FROM access_logs WHERE user_id = u.id AND created_at > NOW() - INTERVAL '1 year') AND NOT EXISTS (SELECT 1 FROM feature_flags ff WHERE ff.user_id = u.id) AND NOT EXISTS (SELECT 1 FROM payment_history ph WHERE ph.user_id = u.id);
 -- delete unused accounts
-DELETE FROM app_users u WHERE NOT EXISTS (SELECT 1 FROM posts WHERE user_id = u.id) AND NOT EXISTS (SELECT 1 FROM projects WHERE user_id = u.id) AND NOT EXISTS (SELECT 1 FROM access_logs WHERE user_id = u.id AND created_at > NOW() - INTERVAL '1 year') AND NOT EXISTS (SELECT 1 FROM feature_flags ff WHERE ff.user_id = u.id AND ff.name = 'plus');
+DELETE FROM app_users u WHERE NOT EXISTS (SELECT 1 FROM posts WHERE user_id = u.id) AND NOT EXISTS (SELECT 1 FROM projects WHERE user_id = u.id) AND NOT EXISTS (SELECT 1 FROM access_logs WHERE user_id = u.id AND created_at > NOW() - INTERVAL '1 year') AND NOT EXISTS (SELECT 1 FROM feature_flags ff WHERE ff.user_id = u.id) AND NOT EXISTS (SELECT 1 FROM payment_history ph WHERE ph.user_id = u.id);
 
 -- how many visits will be deleted
 SELECT count(*) FROM analytics_visits WHERE created_at < NOW() - INTERVAL '1 year';
